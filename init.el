c7f93def43d5080d3471b4d094a968985d46fa33
aaronlin <aaronlin@kkbox.com>
2015-06-28 21:57:49 +0800

    initial commit

---
+++ init.el
@@ -0,0 +1,31 @@
+;; if you're new to the MELPA package manager, just include
+;; this entire snippet in your `~/.emacs` file and follow
+;; the instructions in the comments.
+(require 'package)
+(add-to-list 'package-archives
+         '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+	 '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Restart emacs and do `M-x package-install [RETURN] ensime [RETURN]`
+;; To keep up-to-date, do `M-x list-packages [RETURN] U x [RETURN]`
+
+;; If necessary, make sure "sbt" and "scala" are in the PATH environment
+;; (setenv "PATH" (concat "/path/to/sbt/bin:" (getenv "PATH")))
+;; (setenv "PATH" (concat "/path/to/scala/bin:" (getenv "PATH")))
+;;
+;; On Macs, it might be a safer bet to use exec-path instead of PATH, for instance: 
+;; (setq exec-path (append exec-path '("/usr/local/bin")))
+
+(require 'ensime)
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+
+(require 'evil)
+(evil-mode 1)
+
+(setq linum-format "%d ")
+(global-linum-mode 1)
